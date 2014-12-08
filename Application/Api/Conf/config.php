dd7abf5b9bf919c3b65d16353408778607cb0d8a
weychen <weychen@163.com>
2014-12-08 22:42:23 +0800

    将config中的接口按控制器排序

--- Application/Api/Conf/config.php
+++ Application/Api/Conf/config.php
@@ -63,6 +63,35 @@ return array(
             /// }
             /// 
             array('sessions$','User/login',array('method'=>'POST')),
+            /// 添加预约,各种规则的检查，还别忘了更新预约数量
+            /// $_POST:
+            /// [hospital_id,source_id]
+            /// return :
+            /// {registration表中的不敏感字段，还有其它的跟预约相关的。要求是能完整呈现预约的信息。可以参考挂号网进行一个预约后查看预约信息，它所显示的信息.}
+            array('users/:uid/registrations','User/addRegistration',array('method'=>'POST')),
+            /// 获取用户所有预约
+            /// return:
+            /// [{
+            ///     id,
+            ///     order_at,
+            ///     check_at,
+            ///     hospital_id,
+            ///     user_id,
+            ///     date,
+            ///     doctor_id,
+            ///     status,
+            ///     source_id,
+            ///     code,
+            ///     price
+            /// }]
+            array('users/:uid/registrations','User/getRegistrations',array('method'=>'GET')),
+
+            /// 获取单个预约
+            array('users/:uid/registrations/:rid','User/getRegistrationById',array('method'=>'GET')),
+
+            /// 用户取消预约
+            /// return 所取消的预约.
+            array('users/:uid/registrations/:rid','User/cancelRegistration',array('method'=>'DELETE')),
 
             /// 获取医院列表
             /// $_GET:
@@ -113,38 +142,11 @@ return array(
             ///     amount,
             ///     price
             /// }]
-            array('hospitals/:hid/sources','Hospital/publishSources',array('method'=>'POST')),
+            array('hospitals/:hid/sources','Hospital/publishSources',array('method'=>'POST'))
 	
-	 /// 添加预约,各种规则的检查，还别忘了更新预约数量
-            /// $_POST:
-            /// [hospital_id,source_id]
-            /// return :
-            /// {registration表中的不敏感字段，还有其它的跟预约相关的。要求是能完整呈现预约的信息。可以参考挂号网进行一个预约后查看预约信息，它所显示的信息.}
-	 array('users/:uid/registrations','User/addRegistration',array('method'=>'POST')),
 
-            /// 获取用户所有预约
-            /// return:
-            /// [{
-            ///     id,
-            ///     order_at,
-            ///     check_at,
-            ///     hospital_id,
-            ///     user_id,
-            ///     date,
-            ///     doctor_id,
-            ///     status,
-            ///     source_id,
-            ///     code,
-            ///     price
-            /// }]
-            array('users/:uid/registrations','User/getRegistrations',array('method'=>'GET')),
 
-            /// 获取单个预约
-            array('users/:uid/registrations/:rid','User/getRegistrationById',array('method'=>'GET')),
 
-            /// 用户取消预约
-            /// return 所取消的预约.
-            array('users/:uid/registrations/:rid','User/cancelRegistration',array('method'=>'DELETE'))
 
         ),
 
